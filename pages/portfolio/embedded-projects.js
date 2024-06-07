60bd892c523b1ca2bdf4700b42b4463e928adfce
Umair Ahmadh <umaireravur@gmail.com>
2024-06-07 19:36:27 +0530

    Update embedded-projects.js

--- pages/portfolio/embedded-projects.js
+++ pages/portfolio/embedded-projects.js
@@ -4,12 +4,13 @@ import Design_Portfolio_Section2 from "@/components/sections/homepage/Design_Por
 import Design_Portfolio_Section3 from "@/components/sections/homepage/Design_Portfolio/Design_Portfolio_Section3";
 import Design_Portfolio_Section4 from "@/components/sections/homepage/Design_Portfolio/Design_Portfolio_Section4";
 import Design_Portfolio_Section5 from "@/components/sections/homepage/Design_Portfolio/Design_Portfolio_Section5";
+import Embedded_Portfolio_Banner from "@/components/portfolio/embedded-portfolio-banner";
 
 export default function HomeDesignPortfolio() {
     return (
         <>
             <Layout headerClass="is-transparent sala-dark-scheme" siteContentClass="pt0 pb0">
-                <Design_Portfolio_Section1 />
+                <Embedded_Portfolio_Banner />
                 <Design_Portfolio_Section2 />
                 <Design_Portfolio_Section3 />
                 <Design_Portfolio_Section4 />
