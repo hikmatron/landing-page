a0aafba8eda2cd8726b9274863edcef497f503b9
Umair Ahmadh <umaireravur@gmail.com>
2024-06-07 19:40:46 +0530

    Update embedded-portfolio-banner.js

--- components/portfolio/embedded-portfolio-banner.js
+++ components/portfolio/embedded-portfolio-banner.js
@@ -1,59 +1,32 @@
 import Link from "next/link";
-export default function Embedded_Portfolio_Banner() {
+export default function Embedded_Portfolio_Banner () {
     return (
         <>
-            <section className="section vh100 vh-align-bottom lg-pdt100">
+            <section className="section background-full opt120 spdb relative" style={{ backgroundImage: 'url(/assets/images/hdp-01.png)' }}>
                 <div className="container">
-                    <div className="heading align-center">
-                        <div className="heading-sub">What We Can Help You</div>
-                        <h2 className="heading-title font-second size-l"><i>Think</i> the Design, <br /> Design the <span><i>Thinking</i></span></h2>
+                    <div className="row flex-align-c">
+                        <div className="col-lg-6">
+                            <div className="spdtb pdr50">
+                                <div className="heading heading-alway-white mb32">
+                                    <div className="heading-sub">I'm Kevin Kay</div>
+                                    <h2 className="heading-title size-xl w400">Senior digital designer based in London. Working in UI, UX, and product design.</h2>
+                                </div>
+                                <div className="button-wrap">
+                                    <Link href="/contact-01" className="button fullfield" title="Hire me">Hire me</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6" />
                     </div>
-                    <div className="block-modern-box">
-                        <div className="box wow animate__animated   animate__fadeInDown" data-wow animate__animated-delay="0s">
-                            <div className="inner">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-                                    <path className="elementor-shape-fill" fill="var(--sala-neutral-white, $neutral_white)" d="M1000,4.3V0H0v4.3C0.9,23.1,126.7,99.2,500,100S1000,22.7,1000,4.3z" />
-                                </svg>
-                                <img src="/assets/images/modern-box-01.png" alt="Box" />
-                                <h3 className="title">Ui/Ux<br />Design</h3>
-                            </div>
-                        </div>
-                        <div className="box wow animate__animated   animate__fadeInDown" data-wow animate__animated-delay="0.2s">
-                            <div className="inner">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-                                    <path className="elementor-shape-fill" fill="var(--sala-neutral-white, $neutral_white)" d="M1000,4.3V0H0v4.3C0.9,23.1,126.7,99.2,500,100S1000,22.7,1000,4.3z" />
-                                </svg>
-                                <img src="/assets/images/modern-box-02.png" alt="Box" />
-                                <h3 className="title">Brand<br />Strategy</h3>
-                            </div>
-                        </div>
-                        <div className="box wow animate__animated   animate__fadeInDown" data-wow animate__animated-delay="0.4s">
-                            <div className="inner">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-                                    <path className="elementor-shape-fill" fill="var(--sala-neutral-white, $neutral_white)" d="M1000,4.3V0H0v4.3C0.9,23.1,126.7,99.2,500,100S1000,22.7,1000,4.3z" />
-                                </svg>
-                                <img src="/assets/images/modern-box-03.png" alt="Box" />
-                                <h3 className="title">Print<br />Design</h3>
-                            </div>
-                        </div>
-                        <div className="box wow animate__animated   animate__fadeInDown" data-wow animate__animated-delay="0.6s">
-                            <div className="inner">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-                                    <path className="elementor-shape-fill" fill="var(--sala-neutral-white, $neutral_white)" d="M1000,4.3V0H0v4.3C0.9,23.1,126.7,99.2,500,100S1000,22.7,1000,4.3z" />
-                                </svg>
-                                <img src="/assets/images/modern-box-04.png" alt="Box" />
-                                <h3 className="title">Visual<br />Identity</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="scroll-bar">Scroll</div>
-                    <ul className="socials socials-line">
+                    <ul className="socials socials-line color-white">
                         <li><Link href="/" title="Facebook">Facebook</Link></li>
                         <li><Link href="/" title="Behance">Behance</Link></li>
                         <li><Link href="/" title="Twitter">Twitter</Link></li>
                     </ul>
                 </div>
             </section>
+
+
         </>
     )
 }
