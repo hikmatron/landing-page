e01cd4b8cf7e9587f140b9a71468beb38a8a9ac4
Umair Ahmadh <umaireravur@gmail.com>
2025-04-03 23:09:54 +0530

    Update PortfolioFilterOne.js

--- components/portfolio/PortfolioFilterOne.js
+++ components/portfolio/PortfolioFilterOne.js
@@ -10,7 +10,6 @@ export default function PortfolioFilterOne({ portfolioStyle, portfolioClass }) {
         setTimeout(() => {
             isotope.current = new Isotope(".grid", {
                 itemSelector: ".portfolio-item",
-                // layoutMode: "fitRows",
                 percentPosition: true,
                 masonry: {
                     columnWidth: ".portfolio-item",
@@ -43,229 +42,94 @@ export default function PortfolioFilterOne({ portfolioStyle, portfolioClass }) {
             <div className="portfolio-action">
                 <div className="button-group filter-button-group">
                     <button className={activeBtn("*")} onClick={handleFilterKeyChange("*")}>
-                        All <span>(12)</span>
+                        All
                     </button>
-                    <button className={activeBtn("design")} onClick={handleFilterKeyChange("design")}>Ui/Ux Design <span>(2)</span></button>
-                    <button className={activeBtn("mockup")} onClick={handleFilterKeyChange("mockup")}>
-                        Mockup <span>(4)</span>
+                    <button className={activeBtn("ecommerce")} onClick={handleFilterKeyChange("ecommerce")}>
+                        Ecommerce
                     </button>
-                    <button className={activeBtn("illustrator")} onClick={handleFilterKeyChange("illustrator")}>
-                        Illustrator <span>(2)</span>
+                    <button className={activeBtn("web-app")} onClick={handleFilterKeyChange("web-app")}>
+                        Web App
                     </button>
-                    <button className={activeBtn("branding")} onClick={handleFilterKeyChange("branding")}>
-                        Branding <span>(4)</span>
+                    <button className={activeBtn("mobile-apps")} onClick={handleFilterKeyChange("mobile-apps")}>
+                        Mobile Apps
+                    </button>
+                    <button className={activeBtn("others")} onClick={handleFilterKeyChange("others")}>
+                        Others
                     </button>
                 </div>
             </div>
 
             <div className={`grid ${portfolioStyle ? portfolioStyle : ""}`}>
                 <div className="grid-sizer" />
-                <div className={`portfolio-item grid-item--width2 ${portfolioClass ? portfolioClass : ""} branding`}>
-                    <article className="portfolio ">
+                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} ecommerce`}>
+                    <article className="portfolio">
                         <div className="entry-thumbnail">
-                            <Link href="/" title="Dufa’s Burger">
-                                <img src="/assets/images/portfolio-01.jpg" alt="Dufa’s Burger" />
+                            <Link href="/" title="Ecommerce Project">
+                                <img src="/assets/images/ecommerce.jpg" alt="Ecommerce Project" />
                             </Link>
                         </div>
                         <div className="entry-content">
                             <div className="entry-category">
-                                <Link href="/" title="Branding">Branding</Link>
+                                <Link href="/" title="Ecommerce">Ecommerce</Link>
                             </div>
                             <div className="entry-title">
-                                <h3><Link href="/" title="Dufa’s Burger">Dufa’s Burger 01</Link></h3>
+                                <h3><Link href="/" title="Ecommerce Project">Ecommerce Project</Link></h3>
                             </div>
                         </div>
                     </article>
                 </div>
-                <div className={`portfolio-item grid-item--width3 ${portfolioClass ? portfolioClass : ""} mockup`}>
+                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} web-app`}>
                     <article className="portfolio">
                         <div className="entry-thumbnail">
-                            <Link href="/" title="Sala App">
-                                <img src="/assets/images/portfolio-02.jpg" alt="Sala App" />
+                            <Link href="/" title="Web App Project">
+                                <img src="/assets/images/web-app.jpg" alt="Web App Project" />
                             </Link>
                         </div>
                         <div className="entry-content">
                             <div className="entry-category">
-                                <Link href="/" title="Mockup">Mockup</Link>
+                                <Link href="/" title="Web App">Web App</Link>
                             </div>
                             <div className="entry-title">
-                                <h3><Link href="/" title="Sala App">Sala App 02</Link></h3>
+                                <h3><Link href="/" title="Web App Project">Web App Project</Link></h3>
                             </div>
                         </div>
                     </article>
                 </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} mockup`}>
+                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} mobile-apps`}>
                     <article className="portfolio">
                         <div className="entry-thumbnail">
-                            <Link href="/" title="Food Packaging">
-                                <img src="/assets/images/portfolio-03.jpg" alt="Food Packaging" />
+                            <Link href="/" title="Mobile App Project">
+                                <img src="/assets/images/mobile-app.jpg" alt="Mobile App Project" />
                             </Link>
                         </div>
                         <div className="entry-content">
                             <div className="entry-category">
-                                <Link href="/" title="Mockup">Mockup</Link>
+                                <Link href="/" title="Mobile Apps">Mobile Apps</Link>
                             </div>
                             <div className="entry-title">
-                                <h3><Link href="/" title="Food Packaging">Food Packaging 03</Link></h3>
+                                <h3><Link href="/" title="Mobile App Project">Mobile App Project</Link></h3>
                             </div>
                         </div>
                     </article>
                 </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} branding`}>
+                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} others`}>
                     <article className="portfolio">
                         <div className="entry-thumbnail">
-                            <Link href="/" title="Calendar Design">
-                                <img src="/assets/images/portfolio-04.jpg" alt="Calendar Design" />
+                            <Link href="/" title="Other Project">
+                                <img src="/assets/images/others.jpg" alt="Other Project" />
                             </Link>
                         </div>
                         <div className="entry-content">
                             <div className="entry-category">
-                                <Link href="/" title="Branding">Branding</Link>
+                                <Link href="/" title="Others">Others</Link>
                             </div>
                             <div className="entry-title">
-                                <h3><Link href="/" title="Calendar Design">Calendar Design</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} mockup`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="Packs Premade Scenes">
-                                <img src="/assets/images/portfolio-05.jpeg" alt="Packs Premade Scenes" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Mockup">Mockup</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="Packs Premade Scenes">Packs Premade Scenes</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} branding`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="Magazine Mockup Bundle">
-                                <img src="/assets/images/portfolio-06.jpeg" alt="Magazine Mockup Bundle" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Branding">Branding</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="Magazine Mockup Bundle">Magazine Mockup Bundle</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} design`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="The Golo App">
-                                <img src="/assets/images/portfolio-07.jpeg" alt="The Golo App" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Ui/Ux Design">Ui/Ux Design</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="The Golo App">The Golo App</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} illustrator`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="Yeeow! Illustration Kit">
-                                <img src="/assets/images/portfolio-08.jpeg" alt="Yeeow! Illustration Kit" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Illustrator">Illustrator</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="Yeeow! Illustration Kit">Yeeow! Illustration Kit</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} design`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="Waaahh! Illustration Kit">
-                                <img src="/assets/images/portfolio-09.png" alt="Waaahh! Illustration Kit" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Ui/Ux Design">Ui/Ux Design</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="Waaahh! Illustration Kit">Waaahh! Illustration Kit</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} illustrator`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="Whoosh! Illustration Kit">
-                                <img src="/assets/images/portfolio-10.jpeg" alt="Whoosh! Illustration Kit" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Illustrator">Illustrator</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="Whoosh! Illustration Kit">Whoosh! Illustration Kit</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} mockup`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="Brochure Folder Letter">
-                                <img src="/assets/images/portfolio-11.jpeg" alt="Brochure Folder Letter" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Mockup">Mockup</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="Brochure Folder Letter">Brochure Folder Letter</Link></h3>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-                <div className={`portfolio-item ${portfolioClass ? portfolioClass : ""} branding`}>
-                    <article className="portfolio">
-                        <div className="entry-thumbnail">
-                            <Link href="/" title="Business & Leather Card">
-                                <img src="/assets/images/portfolio-12.jpeg" alt="Business & Leather Card" />
-                            </Link>
-                        </div>
-                        <div className="entry-content">
-                            <div className="entry-category">
-                                <Link href="/" title="Branding">Branding</Link>
-                            </div>
-                            <div className="entry-title">
-                                <h3><Link href="/" title="Business & Leather Card">Business &amp; Leather Card</Link></h3>
+                                <h3><Link href="/" title="Other Project">Other Project</Link></h3>
                             </div>
                         </div>
                     </article>
                 </div>
             </div>
-
         </>
     );
-}; 
+};
